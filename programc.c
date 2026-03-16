c37b646b27f3d9f505de2e8b60f203df5ff3059f
Mateja Kotorčević <mateja.kotorcevic.41.24@ict.edu.rs>
2026-03-16 15:00:21 +0100

    Update programc.c

--- programc.c
+++ programc.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 void main{
-
+  int x;
+  int y;
+  printf("ime i prezime");
+  printf("Index");
+  printf("Unesite godinu:x");
+  scanf_s("%d", x);
+  If y/4==0{
+    printf("godina je prestupna");}
+  else
+  printf("Godina nije prestupna.")
 
 
 }
